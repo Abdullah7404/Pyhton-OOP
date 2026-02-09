e6baa843fb42761203aa85aa041f5b2c90575bd9
Abdullah <ma4830995@gmail.com>
2026-02-09 12:27:32 +0500

    OOP

--- class.py
+++ class.py
@@ -97,3 +97,65 @@ prn = Store("Abdullah", 1000, 10)
 print(prn.material(21))
 prn.purchase(15)
 prn.Total_records()
+
+
+# Simple Class: Ek Student class banao jisme name aur marks attributes hon. Ek method display_info() likho jo student ka naam aur marks print kare.
+
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+display_info = Student("abdullah", 900)
+print(display_info.name)
+print(display_info.marks)
+
+
+# Method Logic: BankAccount class banao jisme balance attribute ho. Usme deposit(amount) aur withdraw(amount) methods hon jo balance ko update karein.
+class BankAccount:
+    def __init__(self,balance):
+        self.balance = balance
+    def deposit(self, amount):
+        self.balance += amount
+        return self.balance
+    def withdraw(self, withdraw):
+        if self.balance >= withdraw:
+            self.balance -= withdraw
+            return self.balance
+        else:
+            return "Balance Nai ha!"
+to_check = BankAccount(30000)
+print(to_check.deposit(3000))
+print(to_check.withdraw(30000))
+
+
+class Name:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+    def Full_Name(self):
+        return (f"The car brand is {self.brand} and model is {self.model}")
+show = Name("BMW", "M5")
+print(show.Full_Name())
+
+
+
+
+#Inheritance
+# Parent Class
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+    def show_details(self):
+        print(f"Employee: {self.name}, Salary: {self.salary}")
+
+# TASK: Ek Child class 'Developer' banao jo 'Employee' ko inherit kare.
+# Usme ek extra attribute 'language' (programming language) ho.
+class Developer(Employee):
+    super().__init(name, salary)
+    def Extra_Language(self, language):
+        self.language = language
+
+detail = Developer("python")
+print(detail.show_details())
